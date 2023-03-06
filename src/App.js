77a32388e58830c7faa2b81a20165bdb57f4e005
therohanmehta <rohandhn786@gmail.com>
2023-03-07 00:50:20 +0530

    we changed and push it to the repository

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Honeysingh } from "./honey";
+
 
 function App() {
   return (
@@ -8,6 +10,10 @@ function App() {
     <h2>heading 2</h2>
     <h3>Heading 3</h3>
     <img src="https://st1.bollywoodlife.com/wp-content/uploads/2014/07/yo-yo-honey-singh-5a.jpg?impolicy=Medium_Widthonly&w=1280&h=900" alt="" />
+    <Honeysingh/>
+    <Honeysingh/>
+    <Honeysingh/>
+    
    </div>
   );
 }
---
+++ src/honey.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+export function Honeysingh(){
+    return(
+        <Fragment>
+
+      <img src="https://st1.bollywoodlife.com/wp-content/uploads/2014/07/yo-yo-honey-singh-5a.jpg?impolicy=Medium_Widthonly&w=1280&h=900" alt="" />
+      <p>this a pic of honey singh</p>
+        </Fragment>
+    )
+  }
